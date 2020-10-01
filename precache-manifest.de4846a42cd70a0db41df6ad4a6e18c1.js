7f4297d79882486e6d725717cc08e3895352d644
jongpal <huni003@hanmail.net>
2020-10-01 20:49:28 +0900

    Updates

---
+++ precache-manifest.de4846a42cd70a0db41df6ad4a6e18c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b64e7334b9f409724058ed91daf3a8",
+    "url": "/first_movie_app/index.html"
+  },
+  {
+    "revision": "31d36139c0a8fec60474",
+    "url": "/first_movie_app/static/css/main.d3a8fc87.chunk.css"
+  },
+  {
+    "revision": "35ad87b045da968bdc03",
+    "url": "/first_movie_app/static/js/2.ba9bf5c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first_movie_app/static/js/2.ba9bf5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d36139c0a8fec60474",
+    "url": "/first_movie_app/static/js/main.1fdd9dde.chunk.js"
+  },
+  {
+    "revision": "5983ecde11bf4be718c0",
+    "url": "/first_movie_app/static/js/runtime-main.0298a7f2.js"
+  }
+]);
